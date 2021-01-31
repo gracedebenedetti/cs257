47e122224bd384bf700b70552a53eefb9a988ba9
Grace de Benedetti <debenedettig@carleton.edu>
2021-01-31 16:17:17 -0700

    first commit

--- olympics-api/api.py
+++ olympics-api/api.py
@@ -14,41 +14,14 @@ import json
 
 app = flask.Flask(__name__)
 
-# Who needs a database when you can just hard-code some actors and movies?
-actors = [
-    {'last_name': 'Pickford', 'first_name': 'Mary'},
-    {'last_name': 'Rains', 'first_name': 'Claude'},
-    {'last_name': 'Lorre', 'first_name': 'Peter'},
-    {'last_name': 'Greenstreet', 'first_name': 'Sydney'},
-    {'last_name': 'Bergman', 'first_name': 'Ingrid'},
-    {'last_name': 'Grant', 'first_name': 'Cary'},
-    {'last_name': 'Colbert', 'first_name': 'Claudette'},
-    {'last_name': 'McDormand', 'first_name': 'Frances'},
-    {'last_name': 'Wiig', 'first_name': 'Kristen'},
-    {'last_name': 'Adams', 'first_name': 'Amy'}
-]
-
-movies = [
-    {'title': 'Casablanca', 'year': 1942, 'genre': 'drama'},
-    {'title': 'North By Northwest', 'year': 1959, 'genre': 'thriller'},
-    {'title': 'Selma', 'year': 2014, 'genre': 'drama'},
-    {'title': 'Alien', 'year': 1979, 'genre': 'scifi'},
-    {'title': 'Bridesmaids', 'year': 2011, 'genre': 'comedy'},
-    {'title': 'Arrival', 'year': 2016, 'genre': 'scifi'},
-    {'title': 'Booksmart', 'year': 2019, 'genre': 'comedy'},
-    {'title': 'It Happened One Night', 'year': 1934, 'genre': 'comedy'},
-    {'title': 'Fargo', 'year': 1996, 'genre': 'thriller'},
-    {'title': 'Clueless', 'year': 1995, 'genre': 'comedy'}
-]
-
 @app.route('/')
 def hello():
     return 'Hello, Citizen of CS257.'
 
-@app.route('/actor/<last_name>')
+@app.route('/games')
 def get_actor(last_name):
     ''' Returns the first matching actor, or an empty dictionary if there's no match. '''
-    actor_dictionary = {}
+    games_dictionary = {}
     lower_last_name = last_name.lower()
     for actor in actors:
         if actor['last_name'].lower().startswith(lower_last_name):
@@ -56,7 +29,33 @@ def get_actor(last_name):
             break
     return json.dumps(actor_dictionary)
 
-@app.route('/movies')
+@app.route('/nocs')
+def get_movies():
+    ''' Returns the list of movies that match GET parameters:
+          start_year, int: reject any movie released earlier than this year
+          end_year, int: reject any movie released later than this year
+          genre: reject any movie whose genre does not match this genre exactly
+        If a GET parameter is absent, then any movie is treated as though
+        it meets the corresponding constraint. (That is, accept a movie unless
+        it is explicitly rejected by a GET parameter.)
+    '''
+    nocs_dictionary = {}
+    id = flask.request.args.get('id')
+    year = flask.request.args.get('year', default=0, type=int)
+    season = flask.request.args.get('season')
+    city = flask.request.args.get('city')
+    for movie in movies:
+        if genre is not None and genre != movie['genre']:
+            continue
+        if movie['year'] < start_year:
+            continue
+        if movie['year'] > end_year:
+            continue
+        movie_list.append(movie)
+
+    return json.dumps(movie_list)
+
+@app.route('/medalists/games/<games_id>?[noc=noc_abbreviation]')
 def get_movies():
     ''' Returns the list of movies that match GET parameters:
           start_year, int: reject any movie released earlier than this year
