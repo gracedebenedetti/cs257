bcd27b535c08ae3292df568fb7a7ac54ae96115e
Quoc Nguyen <nguyenq2@carleton.edu>
2021-02-01 20:03:28 -0600

    2/1/2021, together we accomplished all three endpoints. Next step is to clean up code.

--- olympics-api/api.py
+++ olympics-api/api.py
@@ -12,9 +12,9 @@ import argparse
 import flask
 import json
 import psycopg2
-from config import user
-from config import password
-from config import database
+from quoc_config import user
+from quoc_config import password
+from quoc_config import database
 
 app = flask.Flask(__name__)
 
@@ -29,78 +29,118 @@ def get_game_from_database():
       print(e)
       exit()
   cursor = connection.cursor()
-  query = "SELECT * FROM games"
+  query = "SELECT * FROM games ORDER BY year"
   cursor.execute(query)
-  games_dictionary = {}
+  games_list = []
   for row in cursor:
       one_game = {}
-      game_key = int(row[0]) #need to fix this so the id is actually an int
+      game_id = int(row[0]) 
       year = int(row[1])
       season = row[2]
       city = row[3]
+      one_game['id'] = game_id
       one_game['year'] = year
       one_game['season'] = season
       one_game['city'] = city
-      games_dictionary[game_key] = one_game
-  return games_dictionary
+      games_list.append(one_game)
+  return games_list
 
 @app.route('/games')
 def get_games():
     ''' Returns list of games with their id, year, season, and city. '''
-    games_dictionary = get_game_from_database()
-    return json.dumps(games_dictionary)
-'''
+    games_list = get_game_from_database()
+    return json.dumps(games_list)
+
+
+def get_noc_from_database():
+  try:
+      connection = psycopg2.connect(database=database, user=user, password=password)
+  except Exception as e:
+      print(e)
+      exit()
+  cursor = connection.cursor()
+  query = "SELECT * FROM teams ORDER BY noc"
+  cursor.execute(query)
+  games_list = []
+  for row in cursor:
+      one_game = {}
+      team_id = row[0]
+      noc_abbreviation = row[1] 
+      noc_name = row[2]
+      one_game['id'] = team_id
+      one_game['abbreviation'] = noc_abbreviation
+      one_game['name'] = noc_name
+      games_list.append(one_game)
+  return games_list
+
 @app.route('/nocs')
-def get_movies():
-     Returns the list of movies that match GET parameters:
-          start_year, int: reject any movie released earlier than this year
-          end_year, int: reject any movie released later than this year
-          genre: reject any movie whose genre does not match this genre exactly
-        If a GET parameter is absent, then any movie is treated as though
-        it meets the corresponding constraint. (That is, accept a movie unless
-        it is explicitly rejected by a GET parameter.)
+def get_nocs():
+    nocs_list = get_noc_from_database()
+    return json.dumps(nocs_list)
 
-    nocs_dictionary = {}
-    id = flask.request.args.get('id')
-    year = flask.request.args.get('year', default=0, type=int)
-    season = flask.request.args.get('season')
-    city = flask.request.args.get('city')
-    for movie in movies:
-        if genre is not None and genre != movie['genre']:
-            continue
-        if movie['year'] < start_year:
-            continue
-        if movie['year'] > end_year:
-            continue
-        movie_list.append(movie)
 
-    return json.dumps(movie_list)
+def get_medalists_from_database(games_id):
+  try:
+      connection = psycopg2.connect(database=database, user=user, password=password)
+  except Exception as e:
+      print(e)
+      exit()
+  cursor = connection.cursor()
+  query = "SELECT athletes.id, athletes.name, athletes.sex, sports.sport, "
+  query += "sports.event, athlete_sports_events.medal, athlete_sports_events.games_id "
+  query += "FROM athletes, athlete_sports_events, sports "
+  query += f"WHERE athlete_sports_events.games_id={games_id} " 
+  query += "AND athletes.id=athlete_sports_events.athletes_id "
+  query += "AND athlete_sports_events.sports_id=sports.id"
+  cursor.execute(query)
+  medalists_list = []
+  for row in cursor:
+      one_medalist = {}
+      one_medalist['athlete_id'] = row[0]
+      one_medalist['athlete_name'] = row[1]
+      one_medalist['athlete_sex'] = row[2]
+      one_medalist['sport'] = row[3]
+      one_medalist['event'] = row[4]
+      one_medalist['medal'] = row[5]
+      one_medalist['game_id'] = row[6]
+      medalists_list.append(one_medalist)
+  return medalists_list
 
-@app.route('/medalists/games/<games_id>?[noc=noc_abbreviation]')
-def get_movies():
-    Returns the list of movies that match GET parameters:
-          start_year, int: reject any movie released earlier than this year
-          end_year, int: reject any movie released later than this year
-          genre: reject any movie whose genre does not match this genre exactly
-        If a GET parameter is absent, then any movie is treated as though
-        it meets the corresponding constraint. (That is, accept a movie unless
-        it is explicitly rejected by a GET parameter.)
+def get_medalists_from_database_with_noc(games_id, noc_abbreviation):
+  try:
+      connection = psycopg2.connect(database=database, user=user, password=password)
+  except Exception as e:
+      print(e)
+      exit()
+  cursor = connection.cursor()
+  query = "SELECT athletes.id, athletes.name, athletes.sex, sports.sport, "
+  query += "sports.event, athlete_sports_events.medal "
+  query += "FROM athletes, athlete_sports_events, sports "
+  query += f"WHERE athlete_sports_events.games_id={games_id} " 
+  query += "AND athletes.id=athlete_sports_events.athletes_id "
+  query += f"AND noc='{noc_abbreviation}' "
+  query += "AND athlete_sports_events.sports_id=sports.id"
+  cursor.execute(query)
+  medalists_list = []
+  for row in cursor:
+      one_medalist = {}
+      one_medalist['athlete_id'] = row[0]
+      one_medalist['athlete_name'] = row[1]
+      one_medalist['athlete_sex'] = row[2]
+      one_medalist['sport'] = row[3]
+      one_medalist['event'] = row[4]
+      one_medalist['medal'] = row[5]
+      medalists_list.append(one_medalist)
+  return medalists_list
 
-    movie_list = []
-    genre = flask.request.args.get('genre')
-    start_year = flask.request.args.get('start_year', default=0, type=int)
-    end_year = flask.request.args.get('end_year', default=10000, type=int)
-    for movie in movies:
-        if genre is not None and genre != movie['genre']:
-            continue
-        if movie['year'] < start_year:
-            continue
-        if movie['year'] > end_year:
-            continue
-        movie_list.append(movie)
-
-    return json.dumps(movie_list)
-'''
+@app.route('/medalists/games/<games_id>')
+def get_medalists(games_id):
+    noc_abbreviation = flask.request.args.get('noc')
+    if (noc_abbreviation):
+      medalists_list = get_medalists_from_database_with_noc(games_id, noc_abbreviation)
+    else:
+      medalists_list = get_medalists_from_database(games_id)
+    return json.dumps(medalists_list)
 
 @app.route('/help')
 def get_help():
